ea1a7f3c1eab0552ce569cd06e6f46a11ae6d939
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-08-18 11:02:30 -0400

    add dim labels too

---
+++ models/silver/crossdb/silver__croschain_labels.sql
@@ -0,0 +1,34 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "address",
+    incremental_strategy = 'merge'
+) }}
+
+SELECT
+    system_created_at,
+    insert_date,
+    blockchain,
+    address,
+    creator,
+    label_type,
+    label_subtype,
+    address_name,
+    project_name
+FROM
+    {{ source(
+        'crosschain',
+        'address_labels'
+    ) }}
+WHERE
+    blockchain = 'thorchain'
+
+{% if is_incremental() %}
+AND insert_date >= (
+    SELECT
+        MAX(
+            insert_date
+        )
+    FROM
+        {{ this }}
+)
+{% endif %}
