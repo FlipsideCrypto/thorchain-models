7062c89d9111fbe81044138a1dfecab9d0e6c033
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-11-07 10:00:38 -0500

    tests update

--- tests/tests__fact_pool_block_balances__block_id_assert_no_gaps.sql
+++ tests/tests__fact_pool_block_balances__block_id_assert_no_gaps.sql
@@ -1,1 +1,1 @@
-{{ sequence_distinct_gaps_dim_block_id(ref("corte__fact_pool_block_balances"), "block_id") }}
+{{ sequence_distinct_gaps_dim_block_id(ref("defi__fact_pool_block_balances"), "block_id") }}
--- tests/tests__fact_swaps_assert_all_ids_exist_swap_events.sql
+++
@@ -1,1 +0,0 @@
-{{ all_ids_exist(ref("core__fact_swap_events"), "tx_id", ref("core__fact_swaps"), "tx_id") }}
