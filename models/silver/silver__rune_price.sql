5f82365ce315376465063ba267f9faed47f8f88f
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-07-02 13:44:39 -0400

    .

---
+++ models/silver/silver__rune_price.sql
@@ -0,0 +1,19 @@
+{{ config(
+  materialized = 'view'
+) }}
+
+SELECT
+  rune_price_e8,
+  block_timestamp,
+  DATEADD(
+    ms,
+    __HEVO__LOADED_AT,
+    '1970-01-01'
+  ) AS _INSERTED_TIMESTAMP
+FROM
+  {{ ref(
+    'bronze__rune_price'
+  ) }}
+  e qualify(ROW_NUMBER() over(PARTITION BY block_timestamp
+ORDER BY
+  __HEVO__LOADED_AT DESC)) = 1
